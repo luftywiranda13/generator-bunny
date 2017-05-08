b48fe9a9da92f69140fc173b38081b975a54baa3
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-05-08 21:48:03 +0700

    feat(cli): create prompts

--- generators/app/utils.js
+++
@@ -1,5 +0,0 @@
-const _s = require('underscore.string'); // eslint-disable-line
-const isScoped = require('is-scoped');
-
-exports.repoName = name => (isScoped(name) ? name.split('/')[1] : name);
-exports.slugifyPackageName = name => (isScoped(name) ? name : _s.slugify(name));
