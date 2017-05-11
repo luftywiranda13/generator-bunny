4a6b6c797b0070c7db6fec74e7cf6b212c21262e
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-05-11 14:24:37 +0700

    refactor(dependencies): make codecov to be fully running on Travis CI

--- package-scripts.js
+++ package-scripts.js
@@ -26,10 +26,6 @@ module.exports = {
       default: 'jest --coverage',
       watch: 'jest --watch',
     },
-    reportCoverage: {
-      description: 'Report coverage stats to codecov. This should be run after the `test` script',
-      script: 'codecov',
-    },
     release: {
       description: 'We automate releases with semantic-release. This should only be run on travis',
       script: series(
