8262c5d603dead38b97a3b28c6e42dcceafc3612
Lufty Wiranda <lufty.wiranda@gmail.com>
2017-07-14 15:36:48 +0700

    test: update specs for `readme`

--- app/index.test.js
+++ app/index.test.js
@@ -4,7 +4,7 @@ import assert from 'yeoman-assert';
 import helpers from 'yeoman-test';
 
 describe('Generator', () => {
-  beforeEach(() =>
+  beforeAll(() =>
     helpers.run(path.join(__dirname, '../app')).inTmpDir().withPrompts({
       moduleName: 'bunny module',
       description: 'as cute as bunny',
@@ -41,6 +41,7 @@ describe('Generator', () => {
       'src/index.js',
       'src/index.test.js',
     ]);
+    assert.noFile('yo-rc.json');
   });
 
   describe('templating', () => {
@@ -60,8 +61,81 @@ describe('Generator', () => {
       });
     });
 
-    it('fills installation instructions', () => {
-      assert.fileContent('readme.md', 'npm install --save bunny-module');
+    describe('readme.md', () => {
+      it('fills the title & description', () => {
+        assert.fileContent('readme.md', '# bunny-module');
+        assert.fileContent('readme.md', '> as cute as bunny');
+      });
+
+      describe('badges', () => {
+        it('package version', () => {
+          assert.fileContent(
+            'readme.md',
+            '[![npm](https://img.shields.io/npm/v/bunny-module.svg?style=flat-square)](https://www.npmjs.com/package/bunny-module)'
+          );
+        });
+
+        it('download stats', () => {
+          assert.fileContent(
+            'readme.md',
+            '[![npm](https://img.shields.io/npm/dt/bunny-module.svg?style=flat-square)](https://npm-stat.com/charts.html?package=bunny-module&from=2016-04-01)'
+          );
+        });
+
+        it('build status', () => {
+          assert.fileContent(
+            'readme.md',
+            '[![Travis branch](https://img.shields.io/travis/bunny-module/rust/master.svg?style=flat-square)](https://travis-ci.org/bunny/bunny-module)'
+          );
+        });
+
+        it('coverage', () => {
+          assert.fileContent(
+            'readme.md',
+            '[![Codecov branch](https://img.shields.io/codecov/c/github/bunny/bunny-module/master.svg?style=flat-square)](https://codecov.io/github/bunny/bunny-module)'
+          );
+        });
+
+        it('COC', () => {
+          assert.fileContent(
+            'readme.md',
+            '[![Code of Conduct](https://img.shields.io/badge/code%20of-conduct-ff69b4.svg?style=flat-square)](./other/code_of_conduct.md)'
+          );
+        });
+
+        it('roadmap', () => {
+          assert.fileContent(
+            'readme.md',
+            '[![Roadmap](https://img.shields.io/badge/%F0%9F%93%94-roadmap-CD9523.svg?style=flat-square)](./other/roadmap.md)'
+          );
+        });
+      });
+
+      it('fills the installation instructions', () => {
+        assert.fileContent('readme.md', 'npm install --save bunny-module');
+      });
+
+      it('fills the usage instructions', () => {
+        assert.fileContent(
+          'readme.md',
+          'import bunnyModule from \'bunny-module\''
+        );
+      });
+
+      it('fills the FAQ', () => {
+        assert.fileContent('readme.md', '### Why should I use `bunny-module`?');
+      });
+
+      it('fills the license', () => {
+        assert.fileContent('readme.md', 'MIT &copy; [yo](www.wewww.com)');
+      });
+    });
+
+    it('fiils the license', () => {
+      assert.fileContent(
+        'license',
+        'Copyright (c) yo <hi@bunny.io>'
+      );
     });
   });
 });
